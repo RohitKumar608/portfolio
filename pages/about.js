721c7cdfd2376e011e6a8a5712ebe260a459c6ed
rohitkumarm <rohitkumarrk13568@gmail.com>
2021-01-27 20:43:28 +0530

    feat: added static pages

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/shared/Header';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>I am About Page</h1>
+        <Header />
+      </div>
+    )
+  }
+}
+
+export default About;
---
+++ pages/cv.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Cv extends React.Component {
+  render() {
+    return (
+      <h1>I am Cv Page</h1>
+    )
+  }
+}
+
+export default Cv;
