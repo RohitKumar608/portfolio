6ac65f06ae89acced3437409189ee0a10748e162
rohitkumarm <rohitkumarrk13568@gmail.com>
2021-01-28 09:13:58 +0530

    feat: added custom next-route and portfolio page

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,46 +1,41 @@
-import React from 'react';
-import BaseLayout from '../components/layouts/BaseLayout';
-import Link from 'next/link';
+import React from "react"
+import BaseLayout from "../components/layouts/BaseLayout"
+import { Link } from "../routes"
 
-import axios from 'axios';
+import axios from "axios"
 
 class Portfolios extends React.Component {
-
   static async getInitialProps() {
-    let posts = [];
+    let posts = []
     try {
-      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
-      posts = res.data;
-    } catch(e) {
-      console.error(e);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      posts = res.data
+    } catch (e) {
+      console.error(e)
     }
 
-    return { posts: posts.slice(0, 10) };
+    return { posts: posts.slice(0, 10) }
   }
 
   renderPosts(posts) {
-    return posts.map(post =>
-      <li key={post.id} style={{'fontSize': '20px'}}>
-        <Link as={`/portfolios/${post.id}`} href={`/portfolios/[id]`}>
-          <a>
-            {post.title}
-          </a>
+    return posts.map((post) => (
+      <li key={post.id} style={{ fontSize: "20px" }}>
+        <Link route={`/portfolios/${post.id}`}>
+          <a>{post.title}</a>
         </Link>
       </li>
-    )
+    ))
   }
 
   render() {
-    const { posts } = this.props;
+    const { posts } = this.props
     return (
       <BaseLayout>
         <h1>I am Portfolio Page</h1>
-        <ul>
-          {this.renderPosts(posts)}
-        </ul>
+        <ul>{this.renderPosts(posts)}</ul>
       </BaseLayout>
     )
   }
 }
 
-export default Portfolios;
+export default Portfolios
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const next = require('next');
+const routes = require('./routes');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
-const handle = app.getRequestHandler();
+const handle = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
   const server = express();
